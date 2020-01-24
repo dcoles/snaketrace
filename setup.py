dd3861a5bced9eb5edc3816df28937b6f5669cf5
David Coles <coles.david@gmail.com>
2020-01-23 23:18:09 -0800

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='snaketrace',
-    version='0.1.0',
+    version='0.1.1',
     description='An strace-like tool for Python audit events',
     long_description=long_description,
     long_description_content_type="text/markdown",
